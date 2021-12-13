c564793cff1eec32bcb0ae00038edd9e0baa01e7
natkilima <nathalie.rognon.juyn@gmail.com>
2021-12-13 13:42:41 +0000

    Dictionary component created + search form + handling of search submitted

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <div>
-        <h1>Coucou</h1>
-        <a href="#" className="btn btn-primary">
-          Cliquez ici
-        </a>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Nathalie Rognon</small>
+        </footer>
       </div>
     </div>
   );
